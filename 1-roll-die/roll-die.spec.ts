301fb11a925d60ec97416cdd1c116921f1b68980
Viren Dhanwani <viren@acidtango.com>
2023-05-09 14:35:15 +0100

    test(Die): add test when rolling a die multiple times

--- 1-roll-die/roll-die.spec.ts
+++ 1-roll-die/roll-die.spec.ts
@@ -2,11 +2,27 @@ import { Die } from "./Die";
 
 describe("Roll Die", () => {
   it("rolls a 6 sided die", () => {
+    const ITERATIONS = 100;
     const sixSidedDie = new Die(6);
 
     const rollResult = sixSidedDie.roll();
 
     expect(rollResult).toBeGreaterThan(0);
     expect(rollResult).toBeLessThanOrEqual(6);
+  });
+
+  it("rolls a 6 sided die multiple times", () => {
+    const ITERATIONS = 100;
+    const sixSidedDie = new Die(6);
+
+    let index = 0;
+    while (index < ITERATIONS) {
+      const rollResult = sixSidedDie.roll();
+
+      expect(rollResult).toBeGreaterThan(0);
+      expect(rollResult).toBeLessThanOrEqual(6);
+
+      ++index;
+    }
   });
 });
